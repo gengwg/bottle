92bcc7142876cae5e4700084af2370b8d784af9f
Weigang Geng <gengwg@gmail.com>
2017-06-02 16:02:45 -0700

    more coments

--- bottle_with_gunicorn.py
+++ bottle_with_gunicorn.py
@@ -1,13 +1,17 @@
 from bottle import Bottle, run
 
+# The line sets the "app" object for the WSGI server to run.
 app = Bottle ()
 
+# use @app.route instead of @route
 @app.route('/')
 def index():
     return '<h1>Hello Bottle!</h1>'
 
-if __name__ == "__main__": # must have this for using gunicorn with multiple workers
+# must have this 'main' for using gunicorn with multiple workers
+if __name__ == "__main__":
     run(app, host='localhost', port=8080, reloader=True, debug=True)
 
+# Run it:
 # gunicorn -w 4 -b 127.0.0.1:8080 --log-file=/tmp/gunicorn.log bottle_with_gunicorn:app
 
